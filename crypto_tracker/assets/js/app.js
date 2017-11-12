7c62bf159169c1a824fc83444bd8f33c8bf870e2
Sadruddin Saleem <sadruddinsaleem17@gmail.com>
2017-11-12 10:55:28 -0500

    added jquery, cleaned up JS and added a register link

--- crypto_tracker/assets/js/app.js
+++ crypto_tracker/assets/js/app.js
@@ -19,3 +19,61 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+$(function() {
+
+  if ($("#BTCChart").length > 0) {
+    draw_prices("BTC");
+    draw_prices("ETH");
+    draw_prices("LTC");
+  }
+
+});
+
+
+function draw_prices(currency) {
+    // define GET request for API
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "api/prices?currency=" + currency, true); // true for asynchronous request
+    xmlHttp.send( null );
+
+    // define callback for when api call completes
+    xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState == 4) {
+        if (xmlHttp.status == 200) {
+
+          // parse out data into a list of prices and a list of dates so we can put them in the graph
+          var prices_and_dates = JSON.parse(xmlHttp.responseText);
+          var prices = [];
+          var dates = [];
+
+          var i;
+          for (i = 0; i < prices_and_dates.length; i++) {
+            prices.push(prices_and_dates[i].price);
+            dates.push(prices_and_dates[i].date);
+          }
+
+          // use chart.js to draw the actual chart
+          var ctx = document.getElementById(currency + 'Chart').getContext('2d');
+          var chart = new Chart(ctx, {
+              // The type of chart we want to create
+              type: 'line',
+
+              // The data for our dataset
+              data: {
+                  labels: dates,
+                  datasets: [{
+                      label: currency + " Price Chart",
+                      backgroundColor: 'rgb(255, 99, 132)',
+                      borderColor: 'rgb(255, 99, 132)',
+                      data: prices,
+                  }]
+              },
+
+              // Configuration options go here
+              options: {}
+          });
+        }
+      }
+    }
+}
